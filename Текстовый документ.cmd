e7b4305c718403ca8266b7ba356b4912abf3d541
KishtimGuy <103826442+MatrixStalk@users.noreply.github.com>
2026-09-03 20:11:34 +0500

    total cleaning

--- Текстовый документ.cmd
+++
@@ -1,4 +0,0 @@
-git check-ignore -v bin_x64/
-git check-ignore -v ogsr_engine/xr_3da/ogsr_engine/_TEMP/
-git check-ignore -v ogsr_engine/xr_3da/ogsr_engine/_TEMP/Engine/Release/x64/XR_3DA/pure.obj
-pause
